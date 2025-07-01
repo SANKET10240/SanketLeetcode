3f58c35239375cff0009ffa1cd4b970eb6b2fa73
SANKET KUMAR MOHANTY <116826063+SANKET10240@users.noreply.github.com>
2025-07-01 09:18:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (45.77%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,17 +1,40 @@
-#include <unordered_map>
-
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        unordered_map<char, int> mapS1;
-        for (char c:s1) mapS1[c]++;
-        for (int i=0; i<s2.size(); i++) { 
-            unordered_map<char, int> mapS2;
-            if (mapS1.find(s2[i])!=mapS1.end() && i+s1.size()-1<s2.size()) { 
-                for (int j=i; j<i+s1.size(); j++) mapS2[s2[j]]++;
-                if (mapS2==mapS1) return true;
+
+    bool isFreqSame(vector<int>& freq1, vector<int>& freq2){
+        for(int i = 0; i < 26; i++){
+            if(freq1[i] != freq2[i]){
+                return false;
             }
         }
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+     int m = s1.length();
+     int n = s2.length();
+     if(m > n){
         return false;
+     }   
+
+     vector<int> freq1(26,0), freq2(26,0);
+
+     for(int i = 0; i < m; i++){
+        freq1[s1[i] - 'a']++;
+        freq2[s2[i] - 'a']++;
+     }
+
+     if(isFreqSame(freq1, freq2)){
+        return true;
+     }
+
+     for(int i = m; i < n; i++){
+        freq2[s2[i] - 'a']++;
+        freq2[s2[i - m] - 'a']--;
+
+        if(isFreqSame(freq1, freq2)){
+            return true;
+        }
+     }
+     return false;
     }
 };
