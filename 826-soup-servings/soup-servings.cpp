367cdf99e484045cc36629e246a6b92e7c9ac040
SANKET KUMAR MOHANTY <116826063+SANKET10240@users.noreply.github.com>
2025-08-08 08:23:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (96.83%) - LeetSync

--- 826-soup-servings/soup-servings.cpp
+++ 826-soup-servings/soup-servings.cpp
@@ -1,30 +1,37 @@
+#define db double
 class Solution {
 public:
-    vector<int>optionA={100,75,50,25};
-    vector<int>optionB={0,25,50,75};
+    db dp[193][193];
 
-    double solve(int a, int b, vector<vector<double>> &dp){
-        if(a==0&&b==0)
-        return 0.5;//half the probability that A and B become empty at the same time
-        if(a==0)
-        return 1;
-        if(b==0) return 0; 
-        
-        if(dp[a][b] != -1) return dp[a][b];
-        
-        double ans=0;
-        for(int k=0;k<4;k++){
-            int rema=a-optionA[k];
-            int remb=b-optionB[k];
-            ans =ans+ 0.25*solve(max(0,rema), max(0,remb), dp);
+    db poss(int qA, int qB) {
+        if (qA > 0 && qB <= 0) {
+            return 0;
         }
-        
-        return dp[a][b]=ans;
+        if (qA <= 0 && qB > 0) {
+            return 1;
+        }
+        if (qA <= 0 && qB <= 0) {
+            return 0.5;
+        }
+        if (dp[qA][qB] != -1.0) {
+            return dp[qA][qB];
+        }
+
+        db t1 = 0.25 * poss(qA - 4, qB);
+        t1 += 0.25 * poss(qA - 3, qB - 1);
+        t1 += 0.25 * poss(qA - 2, qB - 2);
+        t1 += 0.25 * poss(qA - 1, qB - 3);
+
+        return dp[qA][qB] = t1;
     }
-    
     double soupServings(int n) {
-        if(n>=4800) return 1;//since we want to calculate the probability of a .so we can see that the a will become 0 at fast rate as we increase n because a is decresing at more faster rate and at sume point probaility reaches 1 for soup A will be empty first .
-        vector<vector<double>> dp(n+1,vector<double>(n+1,-1));
-        return solve(n,n,dp);
+        if (n >= 4800) {
+            return 1;
+        }
+        for (int i = 0; i < 193; ++i)
+            for (int j = 0; j < 193; ++j)
+                dp[i][j] = -1.0;
+        db N = ceil(n / 25.0);
+        return poss(N, N);
     }
 };
