28ab8bf3477e19c20f3684fd749e851f8381df7d
SANKET KUMAR MOHANTY <116826063+SANKET10240@users.noreply.github.com>
2025-10-26 12:42:40 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (12.16%) - LeetSync

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
@@ -1,17 +1,41 @@
 class Solution {
     public long minTime(int[] skill, int[] mana) {
-        int n = skill.length, m = mana.length;
-        long[] done = new long[n + 1];
-        
-        for (int j = 0; j < m; ++j) {
-            for (int i = 0; i < n; ++i) {
-                done[i + 1] = Math.max(done[i + 1], done[i]) + (long) mana[j] * skill[i];
-            }
-            for (int i = n - 1; i > 0; --i) {
-                done[i] = done[i + 1] - (long) mana[j] * skill[i];
-            }
+
+       int n = skill.length;
+        int[] s = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            s[i + 1] = s[i] + skill[i];
         }
         
-        return done[n];
+        int[] suffix = new int[n];
+        int len1 = 0;
+        suffix[len1++] = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (skill[i] > skill[suffix[len1 - 1]]) 
+                suffix[len1++] = i;
+        }
+
+        int[] prefix = new int[n];
+        int len2 = 0;
+        prefix[len2++] = 0;
+        for (int i = 1; i < n; i++) {
+            if (skill[i] > skill[prefix[len2 - 1]]) {
+                prefix[len2++] = i;
+            }
+        }
+
+        int m = mana.length;
+        long start = 0;
+        for (int j = 1; j < m; j++) {
+            int[] indices = mana[j - 1] < mana[j] ? prefix : suffix;
+            int len = mana[j - 1] < mana[j] ? len2 : len1;
+            long max = 0;
+            for (int k = 0; k < len; k++) {
+                int i = indices[k];
+                max = Math.max(max, (long) mana[j - 1] * s[i + 1] - (long) mana[j] * s[i]);
+            }
+            start += max;
+        }
+        return start + (long) mana[m - 1] * s[n];   
     }
 }
